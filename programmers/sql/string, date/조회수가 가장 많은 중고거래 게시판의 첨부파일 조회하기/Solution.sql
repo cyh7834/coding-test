d36c85dfb5d4d0b34f8ce6013d96d0bcea884d5d
Yoonho Choi <cyh7834@gmail.com>
2025-08-17 12:56:39 +0900

    programmers - 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기

---
+++ programmers/sql/string, date/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/Solution.sql
@@ -0,0 +1,16 @@
+SELECT
+    CONCAT('/home/grep/src/', board_id, '/', file_id, file_name, file_ext) AS file_path
+FROM
+    used_goods_file
+WHERE
+    board_id = (
+        SELECT
+            board_id
+        FROM
+            used_goods_board
+        ORDER BY
+            views DESC
+        LIMIT 1
+    )
+ORDER BY
+    file_id DESC;
