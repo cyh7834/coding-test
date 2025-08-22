3bc6ca844fb2046caedc39e3ecdcd82d51942723
Yoonho Choi <cyh7834@gmail.com>
2025-08-22 14:42:01 +0900

    programmers - 대여 기록이 존재하는 자동차 리스트 구하기

---
+++ programmers/sql/string, date/대여 기록이 존재하는 자동차 리스트 구하기/Solution.sql
@@ -0,0 +1,16 @@
+SELECT 
+    car_id
+FROM 
+    car_rental_company_car
+WHERE 
+    car_type = '세단'
+    AND car_id IN (
+        SELECT 
+            car_id
+        FROM 
+            car_rental_company_rental_history
+        WHERE 
+            MONTH(start_date) = 10
+    )
+ORDER BY 
+    car_id DESC;
