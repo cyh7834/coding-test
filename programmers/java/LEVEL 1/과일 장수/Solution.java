1974e3cb206979e16a7c62ed027ef705539413ec
Yoonho Choi <cyh7834@gmail.com>
2025-10-29 13:31:39 +0900

    programmers - 과일 장수

--- programmers/java/LEVEL 1/과일 장수/Solution.java
+++ programmers/java/LEVEL 1/과일 장수/Solution.java
@@ -1,24 +1,14 @@
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        int answerIndex = 0;
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        int scoreLength = score.length;
+        Integer[] boxedScore = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        Arrays.sort(boxedScore, Comparator.reverseOrder());
 
-        for (int i = 0; i < arr1.length; i++) {
-            String[] arr1Binary = getBinaryArray(n, arr1[i]);
-            String[] arr2Binary = getBinaryArray(n, arr2[i]);
-            StringBuilder map = new StringBuilder();
-
-            for (int j = 0; j < arr1Binary.length; j++) {
-                map.append((Integer.parseInt(arr1Binary[j]) + Integer.parseInt(arr2Binary[j])) == 0 ? " " : "#");
-            }
-
-            answer[answerIndex++] = map.toString();
+        for (int i = 0; i < scoreLength - (m - 1); i += m) {
+            answer += boxedScore[i + (m - 1)] * m;
         }
 
         return answer;
-    }
-
-    private String[] getBinaryArray(int n, int num) {
-        return String.format("%" + n + "s", Integer.toBinaryString(num)).replace(' ', '0').split("");
     }
 }
